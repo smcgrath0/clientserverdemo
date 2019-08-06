c2114566f9e086b52d1dac4042a821c882c3905a
smcgrath0 <smcgrath7777@gmail.com>
2019-08-06 12:52:43 -0700

    added dummy grade files with ajax call

--- client/main.js
+++ client/main.js
@@ -16,4 +16,32 @@ function getData(){
 
 function handleDataFromServer( response ){
   console.log(response);
+  getMoreData();
+}
+
+// function addCanidate(){
+//   $.ajax({
+//     url: "../server/getnames.json",
+//     method: 'get',
+//     data: {
+//       name: "john",
+//       grade: "2",
+//     },
+//     dataType: 'json',
+//     success:
+//   })
+// }
+
+function getMoreData(){
+  $.ajax({
+    url: "../server/getgrades.json",
+    method: 'get',
+    dataType: 'json',
+    success: handleMoreDataFromServer,
+  })
+}
+
+
+function handleMoreDataFromServer(response){
+  console.log(response);
 }
